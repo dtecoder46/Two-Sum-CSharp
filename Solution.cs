dc66bd253e9483e9122879dd297ffb513872b0bb
Daniel <dte.biotech46@gmail.com>
2026-03-01 03:20:00 -0500

    Add subtract method and constructors to Solution class
    Refactor Solution class to include subtract method and constructors.

--- Solution.cs
+++ Solution.cs
@@ -1,13 +1,42 @@
 using System;
 
-namespace Solution
+namespace S2;
 {
 	public class Solution
 	{
-
-		public int[] TwoSum(int[] nums, int target)
+	  
+	  private int[] nums;
+	  private int target;
+	  
+	  public int[] subtract(int[] nums, int target, int index) {
+	      int difference = target - nums[index];
+	      
+	      int matchingIndex = Array.IndexOf(nums, difference);
+	      
+	      if (matchingIndex > -1) {
+	          int[] indices = {index, matchingIndex};
+	          return indices
+	      }
+	      else {
+	        index += 1;
+	      }
+	  }
+	  
+	  public Solution() {
+	      nums = {};
+	      target = 0;
+	  }
+	  
+	  public Solution(int[] inputNums, int inputTarget) {
+	      this.nums = inputNums;
+	      this.target = inputTarget;
+	  }
+	  
+		public int[] TwoSum()
 		{
-			return "Hello from New Class 1!";
+		    int index = 0;
+		    int[] arrayIndices = subtract(this.nums, this.target, index);
+		    return arrayIndices;
 		}
 	}
 }
